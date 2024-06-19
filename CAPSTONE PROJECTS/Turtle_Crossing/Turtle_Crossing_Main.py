bf1b66e985d8e2ee5ac01f0d8554db3c7b5fbacb
Afsheen Wasfiya Abdul Wahab <120241509+Afkeen2004@users.noreply.github.com>
2024-06-19 18:40:33 +0530

    Add files via upload

---
+++ CAPSTONE PROJECTS/Turtle_Crossing/Turtle_Crossing_Main.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen
+from Turtle_Crossing_Player import Player
+from Turtle_Crossing_Car_Manager import CarManager
+from Turtle_Crossing_Scoreboard import Scoreboard
+
+#setting up screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+#class instances
+score = Scoreboard()
+car_manager = CarManager()
+player = Player()
+
+#listens for key presses
+screen.listen()
+screen.onkeypress(player.go_up, "Up")
+
+#game start
+game_is_on = True
+while game_is_on:
+    #screen updation
+    time.sleep(0.1)
+    screen.update()
+
+    #cars instances
+    car_manager.create_car()
+    car_manager.move_car()
+
+    #checking for collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    #next level if finish line reached
+    if player.is_at_finish_line():
+        player.go_to_start() #go to start position
+        car_manager.level_up_car() #faster cars
+        score.increase_level() #increase score
+
+screen.exitonclick()
